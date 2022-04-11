f319126926997bdad2774b01a4c236d7fb45ecd2
Heylga <oy.golovina@gmail.com>
2022-04-11 13:51:54 +0000

    onClick event created

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,15 +13,18 @@ const Home = () => {
 			<div className="stick"></div>
 			<div className="traffic-light">
 				<div
+					onClick={() => setSelectedColor("red")}
 					className={
 						"light red" + (selectedColor === "red" ? "glow" : "")
 					}></div>
 				<div
+					onClick={() => setSelectedColor("yellow")}
 					className={
 						"light yellow" +
 						(selectedColor === "yellow" ? "glow" : "")
 					}></div>
 				<div
+					onClick={() => setSelectedColor("green")}
 					className={
 						"light green" +
 						(selectedColor === "green" ? "glow" : "")
